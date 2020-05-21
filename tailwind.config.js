c61284827abfdd9eb63d7f4cecca68126011d628
Heath Harrelson <heath.harrelson@gmail.com>
2020-05-21 13:07:53 -0700

    Import code from Phoenix project
    Import HTML, assets, and configuration from the Phoenix-based
    project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,53 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  purge: [
+    './public/**/*.html'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        '6': '6px',
+      },
+      colors: {
+        // override Tailwind UI colors with the defaults
+        ...defaultTheme.colors,
+        mp: {
+          100: 'hsl(116, 0%, 95%)',
+          200: 'hsl(116, 0%, 92%)',
+          300: 'hsl(116, 0%, 89%)',
+          800: 'hsl(116, 0%, 16%)',
+          900: 'hsl(116, 0%, 13%)',
+        },
+      },
+      maxHeight: {
+        '968': '968px',
+        '968x2': '1936px',
+      },
+      maxWidth: {
+        '1/4': '25%',
+        '1/2': '50%',
+        '7/10': '70%',
+        '3/4': '75%',
+      },
+      fontFamily: {
+        sans: [
+          'Roboto',
+          ...defaultTheme.fontFamily.sans
+        ],
+        condensed: ['"Roboto Condensed"'],
+      },
+      screens: {
+        'bs-sm': '576px',
+        'bs-md': '768px',
+        'bs-lg': '992px',
+        'bs-xl': '1200px',
+      }
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    // for new sizes, mostly
+    require('@tailwindcss/ui')
+  ],
 }
